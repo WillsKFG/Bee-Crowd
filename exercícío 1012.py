56b84c881b7822120558383ae219fdd43937a0d8
Kauan <159711116+WillsKFG@users.noreply.github.com>
2025-03-19 20:01:19 -0300

    mudei os nome

--- exercícío 1012.py
+++ exercícío 1012.py
@@ -4,3 +4,20 @@
 #c) a área do trapézio que tem A e B por bases e C por altura.
 #d) a área do quadrado que tem lado B.
 #e) a área do retângulo que tem lados A e B.
+
+entrada =  input().split()
+A = float(entrada[0])
+B = float(entrada[1])
+C = float(entrada[2])
+
+area_triangulo = A * C/2
+area_circulo = 3.14159 * C**2
+area_trapezio = (A+B)*C/2
+area_quadrado = B*B
+area_retangulo = A*B
+
+print(f'''TRIANGULO: {area_triangulo:.3f}
+CIRCULO: {area_circulo:.3f}
+TRAPEZIO: {area_trapezio:.3f}
+QUADRADO: {area_quadrado:.3f}
+RETANGULO: {area_retangulo:.3f}''')
